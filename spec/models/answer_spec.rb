aea6ad6913d9a25ef4608445d97fe716f67c8269
Riley Westbrook <westbrookriley@gmail.com>
2017-12-04 18:31:41 -0500

    assignment 14 complete

---
+++ spec/models/answer_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Answer, type: :model do
+  let(:question) { Question.create!(title: "New Question Title", body: "New Question Body") }
+  let(:answer) { Answer.create!(body: 'Answer Body', question: question) }
+
+  it "responds to body" do
+    expect(answer).to respond_to(:body)
+  end
+end
