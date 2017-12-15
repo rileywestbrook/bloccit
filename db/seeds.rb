af6bc2d22322730e80f5fcfed0966e4c235e101c
Riley Westbrook <westbrookriley@gmail.com>
2017-12-15 10:48:32 -0500

    checkpoint 21 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,25 @@
 require 'random_data'
 
-#Creat posts
+# Create Topics
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
+# Create posts
 50.times do
   Post.create!(
+    topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
    )
 end
 posts = Post.all
 
-#Create comments
+# Create comments
 100.times do
   Comment.create!(
     post: posts.sample,
@@ -18,5 +28,6 @@ posts = Post.all
 end
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
