e5b265804fc010efbdaf290c1dcece65220efb59
Riley Westbrook <westbrookriley@gmail.com>
2018-01-04 16:27:53 -0500

    assignment 30 complete

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -13,4 +13,13 @@ class FavoriteMailer < ApplicationMailer
 
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<comments/#{comment.id}@infinite-brook-27301.herokuapp.com>"
+    headers["In-Reply-To"] = "<post/#{post.id}@infinite-brook-27301.herokuapp.com>"
+    headers["References"] = "<post/#{post.id}@infinite-brook-27301.herokuapp.come>"
+
+    @post = post
+    mail(to: post.user.email, subject: "You're following #{post.title}")
+  end
 end
