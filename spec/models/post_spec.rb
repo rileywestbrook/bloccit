045885ecbb5e763616f121d13c2fc4bde51f4f99
Riley Westbrook <westbrookriley@gmail.com>
2018-01-04 18:14:21 -0500

    checkpoint 31 complete

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
+  let(:topic) { create(:topic) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
 
   it { is_expected.to have_many(:comments) }
   it { is_expected.to have_many(:votes) }
@@ -22,7 +22,7 @@ RSpec.describe Post, type: :model do
 
   describe "attributes" do
     it "has a title and body attribute" do
-      expect(post).to have_attributes(title: title, body: body)
+      expect(post).to have_attributes(title: post.title, body: post.body)
     end
   end
 
